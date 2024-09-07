3fb0ad53958c5b373e35195845c20e2a7d730a0d
kenny <kenny.kwyiu@gmail.com>
2024-09-07 19:16:17 +0800

    MyBatis Mapper - MyMapper

--- foodie-mapper/src/main/java/com/kenny/my/mapper/MyMapper.java
+++ foodie-mapper/src/main/java/com/kenny/my/mapper/MyMapper.java
@@ -28,7 +28,7 @@ import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
 
 /**
- * 继承自己的MyMapper
+ * Inherit your own MyMapper
  */
 public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T> {
 }
