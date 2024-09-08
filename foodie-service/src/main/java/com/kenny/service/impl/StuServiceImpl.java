0e85f86c7681fe8c49a0b120c23f074a85888f4a
kenny <kenny.kwyiu@gmail.com>
2024-09-08 15:10:47 +0800

    added controller - finished 2-22

--- foodie-service/src/main/java/com/kenny/service/impl/StuServiceImpl.java
+++ foodie-service/src/main/java/com/kenny/service/impl/StuServiceImpl.java
@@ -5,29 +5,45 @@ import com.kenny.pojo.Stu;
 import com.kenny.service.StuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StuServiceImpl implements StuService {
 
     @Autowired
     private StuMapper stuMapper;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
     @Override
     public Stu getStuInfo(int id) {
         return stuMapper.selectByPrimaryKey(id);
     }
 
+
+
+    @Transactional(propagation = Propagation.REQUIRED)
     @Override
     public void saveStu() {
-
+        Stu stu = new Stu();
+        stu.setName("jack");
+        stu.setAge(22);
+        stuMapper.insert(stu);
     }
 
+    @Transactional(propagation = Propagation.REQUIRED)
     @Override
     public void updateStu(int id) {
-
+        Stu stu = new Stu();
+        stu.setId(id);
+        stu.setName("lucy");
+        stu.setAge(24);
+        stuMapper.updateByPrimaryKey(stu);
     }
 
+    @Transactional(propagation = Propagation.REQUIRED)
     @Override
     public void deleteStu(int id) {
-
+        stuMapper.deleteByPrimaryKey(id);
     }
 }
