760b2c7ab55b6eb4d97355461e536de77599e984
kenny <kenny.kwyiu@gmail.com>
2024-09-09 22:13:49 +0800

    usernameIsExist - revised

--- foodie-api/src/main/java/com/kenny/controller/PassportController.java
+++ foodie-api/src/main/java/com/kenny/controller/PassportController.java
@@ -3,6 +3,7 @@ package com.kenny.controller;
 import com.kenny.service.UserService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,16 +16,16 @@ public class PassportController {
     private UserService userService;
 
     @GetMapping("/usernameIsExist")
-    public int usernameIsExist(@RequestParam String username) {
+    public HttpStatus usernameIsExist(@RequestParam String username) {
         if (StringUtils.isBlank(username)) {
-            return 500;
+            return HttpStatus.INTERNAL_SERVER_ERROR;
         }
 
         boolean isExist = userService.queryUsernameIsExist(username);
         if (isExist) {
-            return 500;
+            return HttpStatus.INTERNAL_SERVER_ERROR;
         }
 
-        return 200;
+        return HttpStatus.OK;
     }
 }
