08feeaae85a762681a3a7e0e73a5be5a515f91c0
kenny <kenny.kwyiu@gmail.com>
2024-09-09 21:44:48 +0800

    UserServiceImpl

---
+++ foodie-service/src/main/java/com/kenny/service/UserService.java
@@ -0,0 +1,8 @@
+package com.kenny.service;
+
+import com.kenny.pojo.Stu;
+
+public interface UserService {
+    public boolean queryUsernameIsExist(String username);
+
+}
---
+++ foodie-service/src/main/java/com/kenny/service/impl/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package com.kenny.service.impl;
+
+import com.kenny.mapper.StuMapper;
+import com.kenny.mapper.UsersMapper;
+import com.kenny.pojo.Users;
+import com.kenny.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.mapper.entity.Example;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UsersMapper usersMapper;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public boolean queryUsernameIsExist(String username) {
+
+        Example userExample = new Example(Users.class);
+        Example.Criteria userCriteria = userExample.createCriteria();
+
+        userCriteria.andEqualTo("username", username);
+
+        Users result = usersMapper.selectOneByExample(userExample);
+
+        return result == null ? false : true;
+    }
+}
