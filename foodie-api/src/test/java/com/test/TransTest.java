42ee25f07f039c4fcfcc748be6b700be6d1705e9
kenny <kenny.kwyiu@gmail.com>
2024-09-09 21:35:31 +0800

    Transaction Propagation - TransTest

---
+++ foodie-api/src/test/java/com/test/TransTest.java
@@ -0,0 +1,25 @@
+package com.test;
+
+import com.kenny.Application;
+import com.kenny.service.StuService;
+import com.kenny.service.TestTransService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class TransTest {
+    @Autowired
+    private StuService stuService;
+
+    @Autowired
+    private TestTransService testTransService;
+
+    @Test
+    public void myTest(){
+        testTransService.testPropagationTrans();
+    }
+}
