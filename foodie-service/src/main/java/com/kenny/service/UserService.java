71459b9239c6eb8c149d3c8130d439e4f2cdc57d
kenny <kenny.kwyiu@gmail.com>
2024-09-10 22:06:38 +0800

    createUser

--- foodie-service/src/main/java/com/kenny/service/UserService.java
+++ foodie-service/src/main/java/com/kenny/service/UserService.java
@@ -1,8 +1,12 @@
 package com.kenny.service;
 
+import com.kenny.bo.UserBO;
 import com.kenny.pojo.Stu;
+import com.kenny.pojo.Users;
 
 public interface UserService {
     public boolean queryUsernameIsExist(String username);
+
+    public Users createUser(UserBO userBO);
 
 }
--- foodie-service/src/main/java/com/kenny/service/impl/UserServiceImpl.java
+++ foodie-service/src/main/java/com/kenny/service/impl/UserServiceImpl.java
@@ -1,20 +1,33 @@
 package com.kenny.service.impl;
 
+import com.kenny.bo.UserBO;
+import com.kenny.enums.Sex;
 import com.kenny.mapper.StuMapper;
 import com.kenny.mapper.UsersMapper;
 import com.kenny.pojo.Users;
 import com.kenny.service.UserService;
+import com.kenny.utils.DateUtil;
+import com.kenny.utils.MD5Utils;
+import org.n3r.idworker.Sid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UsersMapper usersMapper;
+
+    @Autowired
+    private Sid sid;
+
+    private static final String USER_FACE = "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745";
 
     @Transactional(propagation = Propagation.SUPPORTS)
     @Override
@@ -28,5 +41,33 @@ public class UserServiceImpl implements UserService {
         Users result = usersMapper.selectOneByExample(userExample);
 
         return result == null ? false : true;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public Users createUser(UserBO userBO) {
+
+        String userId = sid.nextShort();
+
+        Users user = new Users();
+        user.setId(userId);
+        user.setUsername(userBO.getUsername());
+
+        try {
+            user.setPassword(MD5Utils.getMD5Str(userBO.getPassword()));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+        user.setNickname(userBO.getUsername());
+        user.setFace(USER_FACE);
+        user.setBirthday(DateUtil.stringToDate("1970-01-01"));
+        user.setSex(Sex.secret.type);
+        user.setCreatedTime(new Date());
+        user.setUpdatedTime(new Date());
+
+        usersMapper.insert(user);
+
+        return user;
     }
 }
