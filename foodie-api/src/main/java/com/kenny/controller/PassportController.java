b71498e861507aebf6298dab36db3ba2ec6c4061
kenny <kenny.kwyiu@gmail.com>
2024-09-11 20:46:42 +0800

    Swagger2 - add ApiModel

--- foodie-api/src/main/java/com/kenny/controller/PassportController.java
+++ foodie-api/src/main/java/com/kenny/controller/PassportController.java
@@ -3,17 +3,20 @@ package com.kenny.controller;
 import com.kenny.bo.UserBO;
 import com.kenny.service.UserService;
 import com.kenny.utils.JsonResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@Api(value = "register login", tags = {"api for register and login"})
 @RestController
 @RequestMapping("passport")
 public class PassportController {
     @Autowired
     private UserService userService;
 
+    @ApiOperation(value = "is username exist or not", notes = "is username exist or not", httpMethod = "GET")
     @GetMapping("/usernameIsExist")
     public JsonResult usernameIsExist(@RequestParam String username) {
         if (StringUtils.isBlank(username)) {
@@ -27,6 +30,8 @@ public class PassportController {
 
         return JsonResult.ok();
     }
+
+    @ApiOperation(value = "user register", notes = "user register", httpMethod = "POST")
     @PostMapping("/register")
     public JsonResult register(@RequestBody UserBO userBO) {
         String username = userBO.getUsername();
