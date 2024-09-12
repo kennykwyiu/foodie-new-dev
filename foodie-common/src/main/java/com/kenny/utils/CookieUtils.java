8a782342595a493d21d181ae564c4b52ac42b281
kenny <kenny.kwyiu@gmail.com>
2024-09-12 20:54:18 +0800

    CookieUtils

---
+++ foodie-common/src/main/java/com/kenny/utils/CookieUtils.java
@@ -0,0 +1,297 @@
+package com.kenny.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+
+/**
+ * @Description: Cookie Utility Class
+ */
+public final class CookieUtils {
+
+    final static Logger logger = LoggerFactory.getLogger(CookieUtils.class);
+
+    /**
+     * @param request
+     * @param cookieName
+     * @return
+     * @Description: Get the value of a Cookie without encoding
+     */
+    public static String getCookieValue(HttpServletRequest request, String cookieName) {
+        return getCookieValue(request, cookieName, false);
+    }
+
+    /**
+     * @param request
+     * @param cookieName
+     * @param isDecoder
+     * @return
+     * @Description: Get the value of a Cookie
+     */
+    public static String getCookieValue(HttpServletRequest request, String cookieName, boolean isDecoder) {
+        Cookie[] cookieList = request.getCookies();
+        if (cookieList == null || cookieName == null) {
+            return null;
+        }
+        String retValue = null;
+        try {
+            for (int i = 0; i < cookieList.length; i++) {
+                if (cookieList[i].getName().equals(cookieName)) {
+                    if (isDecoder) {
+                        retValue = URLDecoder.decode(cookieList[i].getValue(), "UTF-8");
+                    } else {
+                        retValue = cookieList[i].getValue();
+                    }
+                    break;
+                }
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return retValue;
+    }
+
+    /**
+     * @param request
+     * @param cookieName
+     * @param encodeString
+     * @return
+     * @Description: Get the value of a Cookie
+     */
+    public static String getCookieValue(HttpServletRequest request, String cookieName, String encodeString) {
+        Cookie[] cookieList = request.getCookies();
+        if (cookieList == null || cookieName == null) {
+            return null;
+        }
+        String retValue = null;
+        try {
+            for (int i = 0; i < cookieList.length; i++) {
+                if (cookieList[i].getName().equals(cookieName)) {
+                    retValue = URLDecoder.decode(cookieList[i].getValue(), encodeString);
+                    break;
+                }
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return retValue;
+    }
+
+    /**
+     * @param request
+     * @param response
+     * @param cookieName
+     * @param cookieValue
+     * @Description: Set the value of a Cookie without setting expiration time (expires when browser is closed), and without encoding
+     */
+    public static void setCookie(HttpServletRequest request, HttpServletResponse response, String cookieName,
+                                 String cookieValue) {
+        setCookie(request, response, cookieName, cookieValue, -1);
+    }
+
+    /**
+     * @param request
+     * @param response
+     * @param cookieName
+     * @param cookieValue
+     * @param cookieMaxage
+     * @Description: Set the value of a Cookie with specified expiration time, but without encoding
+     */
+    public static void setCookie(HttpServletRequest request, HttpServletResponse response, String cookieName,
+                                 String cookieValue, int cookieMaxage) {
+        setCookie(request, response, cookieName, cookieValue, cookieMaxage, false);
+    }
+
+    /**
+     * @param request
+     * @param response
+     * @param cookieName
+     * @param cookieValue
+     * @param isEncode
+     * @Description: Set the value of a Cookie without setting expiration time, but with encoding
+     */
+    public static void setCookie(HttpServletRequest request, HttpServletResponse response, String cookieName,
+                                 String cookieValue, boolean isEncode) {
+        setCookie(request, response, cookieName, cookieValue, -1, isEncode);
+    }
+
+    /**
+     * @param request
+     * @param response
+     * @param cookieName
+     * @param cookieValue
+     * @param cookieMaxage
+     * @param isEncode
+     * @Description: Set the value of a Cookie with specified expiration time, and encode the value
+     */
+    public static void setCookie(HttpServletRequest request, HttpServletResponse response, String cookieName,
+                                 String cookieValue, int cookieMaxage, boolean isEncode) {
+        doSetCookie(request, response, cookieName, cookieValue, cookieMaxage, isEncode);
+    }
+
+    /**
+     * @param request
+     * @param response
+     * @param cookieName
+     * @param cookieValue
+     * @param cookieMaxage
+     * @param encodeString
+     * @Description: Set the value of a Cookie with specified expiration time, and encode the value using a specified encoding
+     */
+    public static void setCookie(HttpServletRequest request, HttpServletResponse response, String cookieName,
+                                 String cookieValue, int cookieMaxage, String encodeString) {
+        doSetCookie(request, response, cookieName, cookieValue, cookieMaxage, encodeString);
+    }
+
+    /**
+     * @param request
+     * @param response
+     * @param cookieName
+     * @Description: Delete a Cookie with the specified cookie name
+     */
+    public static void deleteCookie(HttpServletRequest request, HttpServletResponse response,
+                                    String cookieName) {
+        doSetCookie(request, response, cookieName, null, -1, false);
+//        doSetCookie(request, response, cookieName, "", -1, false);
+    }
+
+    /**
+     * @param request
+     * @param response
+     * @param cookieName
+     * @param cookieValue
+     * @param cookieMaxage Maximum number of seconds the cookie is effective
+     * @param isEncode
+     * @Description: Set the value of a Cookie and make it effective for a specified time
+     */
+    private static final void doSetCookie(HttpServletRequest request, HttpServletResponse response,
+                                          String cookieName, String cookieValue, int cookieMaxage, boolean isEncode) {
+        try {
+            if (cookieValue == null) {
+                cookieValue = "";
+            } else if (isEncode) {
+                cookieValue = URLEncoder.encode(cookieValue, "utf-8");
+            }
+            Cookie cookie = new Cookie(cookieName, cookieValue);
+            if (cookieMaxage > 0)
+                cookie.setMaxAge(cookieMaxage);
+            if (null != request) {
+                String domainName = getDomainName(request);
+                logger.info("========== domainName: {} ==========", domainName);
+                if (!"localhost".equals(domainName)) {
+                    cookie.setDomain(domainName);
+                }
+            }
+            cookie.setPath("/");
+            response.addCookie(cookie);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param request
+     * @param response
+     * @param cookieName
+     * @param cookieValue
+     * @param cookieMaxage Maximum number of seconds the cookie is effective
+     * @param encodeString
+     * @Description: Set the value of a Cookie and make it effective for a specified time with encoding using a specified string
+     */
+    private static final void doSetCookie(HttpServletRequest request, HttpServletResponse response,
+                                          String cookieName, String cookieValue, int cookieMaxage, String encodeString) {
+        try {
+            if (cookieValue == null) {
+                cookieValue = "";
+            } else {
+                cookieValue = URLEncoder.encode(cookieValue, encodeString);
+            }
+            Cookie cookie = new Cookie(cookieName, cookieValue);
+            if (cookieMaxage > 0)
+                cookie.setMaxAge(cookieMaxage);
+            if (null != request) {
+                String domainName = getDomainName(request);
+                logger.info("========== domainName: {} ==========", domainName);
+                if (!"localhost".equals(domainName)) {
+                    cookie.setDomain(domainName);
+                }
+            }
+            cookie.setPath("/");
+            response.addCookie(cookie);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @return
+     * @Description: Get the domain name of the cookie
+     */
+    private static final String getDomainName(HttpServletRequest request) {
+        String domainName = null;
+
+        String serverName = request.getRequestURL().toString();
+        if (serverName == null || serverName.equals("")) {
+            domainName = "";
+        } else {
+            serverName = serverName.toLowerCase();
+            serverName = serverName.substring(7);
+            final int end = serverName.indexOf("/");
+            serverName = serverName.substring(0, end);
+            if (serverName.indexOf(":") > 0) {
+                String[] ary = serverName.split("\\:");
+                serverName = ary[0];
+            }
+
+            final String[] domains = serverName.split("\\.");
+            int len = domains.length;
+            if (len > 3 && !isIp(serverName)) {
+                // www.xxx.com.cn
+                domainName = "." + domains[len - 3] + "." + domains[len - 2] + "." + domains[len - 1];
+            } else if (len <= 3 && len > 1) {
+                // xxx.com or xxx.cn
+                domainName = "." + domains[len - 2] + "." + domains[len - 1];
+            } else {
+                domainName = serverName;
+            }
+        }
+        return domainName;
+    }
+
+    public static String trimSpaces(String IP) {
+        // Remove leading spaces
+        while (IP.startsWith(" ")) {
+            IP = IP.substring(1, IP.length()).trim();
+        }
+
+        // Remove trailing spaces
+        while (IP.endsWith(" ")) {
+            IP = IP.substring(0, IP.length() - 1).trim();
+        }
+        return IP;
+    }
+
+    public static boolean isIp(String IP) {
+        boolean b = false;
+        IP = trimSpaces(IP);
+
+        // Check if the string matches the IP address pattern
+        if (IP.matches("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}")) {
+            String s[] = IP.split("\\.");
+            if (Integer.parseInt(s[0]) < 255)
+                if (Integer.parseInt(s[1]) < 255)
+                    if (Integer.parseInt(s[2]) < 255)
+                        if (Integer.parseInt(s[3]) < 255)
+                            b = true;
+        }
+        return b;
+    }
+
+}
