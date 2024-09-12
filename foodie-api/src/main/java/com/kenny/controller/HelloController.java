e93e0e5b2aa5d0dd9bd6566995e4cf5f2e676d15
kenny <kenny.kwyiu@gmail.com>
2024-09-12 20:54:08 +0800

    setSession for testing

--- foodie-api/src/main/java/com/kenny/controller/HelloController.java
+++ foodie-api/src/main/java/com/kenny/controller/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @ApiIgnore
 @RestController
 public class HelloController {
@@ -12,4 +15,13 @@ public class HelloController {
         return "</br></br></br><H1>Hello World!</H1>";
     }
 
+        @GetMapping("/setSession")
+    public Object setSession(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.setAttribute("userInfo", "new user");
+        session.setMaxInactiveInterval(3600);
+        session.getAttribute("userInfo");
+//        session.removeAttribute("userInfo");
+        return "ok";
+    }
 }
