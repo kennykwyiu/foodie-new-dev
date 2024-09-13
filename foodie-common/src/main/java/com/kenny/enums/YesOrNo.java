b0a5b10ee79d92463658b37bcab06dfdf2260d6f
kenny <kenny.kwyiu@gmail.com>
2024-09-13 21:50:19 +0800

    YesOrNo enum

---
+++ foodie-common/src/main/java/com/kenny/enums/YesOrNo.java
@@ -0,0 +1,14 @@
+package com.kenny.enums;
+
+public enum YesOrNo {
+    NO(0, "NO"),
+    YES(1, "YES");
+
+    public final Integer type;
+    public final String value;
+
+    YesOrNo(Integer type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+}
