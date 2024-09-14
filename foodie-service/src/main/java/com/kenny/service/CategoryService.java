0fc3d4e16a5d5f095f2417a1c708e94299570197
kenny <kenny.kwyiu@gmail.com>
2024-09-14 14:01:24 +0800

    queryAllRootLevelCat in CategoryServiceImpl

--- foodie-service/src/main/java/com/kenny/service/CategoryService.java
+++ foodie-service/src/main/java/com/kenny/service/CategoryService.java
@@ -2,9 +2,13 @@ package com.kenny.service;
 
 import com.kenny.pojo.Carousel;
 import com.kenny.pojo.Category;
+import com.kenny.vo.CategoryVO;
+import io.swagger.models.auth.In;
 
 import java.util.List;
 
 public interface CategoryService {
     public List<Category> queryAllRootLevelCat();
+
+    public List<CategoryVO> getSubCatList(Integer rootCatId);
 }
--- foodie-service/src/main/java/com/kenny/service/impl/CategoryServiceImpl.java
+++ foodie-service/src/main/java/com/kenny/service/impl/CategoryServiceImpl.java
@@ -2,12 +2,16 @@ package com.kenny.service.impl;
 
 import com.kenny.mapper.CarouselMapper;
 import com.kenny.mapper.CategoryMapper;
+import com.kenny.mapper.CategoryMapperCustom;
 import com.kenny.pojo.Carousel;
 import com.kenny.pojo.Category;
 import com.kenny.service.CarouselService;
 import com.kenny.service.CategoryService;
+import com.kenny.vo.CategoryVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
@@ -17,7 +21,10 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Autowired
     private CategoryMapper categoryMapper;
+    @Autowired
+    private CategoryMapperCustom categoryMapperCustom;
 
+    @Transactional(propagation = Propagation.SUPPORTS)
     @Override
     public List<Category> queryAllRootLevelCat() {
         Example example = new Example(Category.class);
@@ -26,5 +33,12 @@ public class CategoryServiceImpl implements CategoryService {
 
         List<Category> categories = categoryMapper.selectByExample(example);
         return categories;
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<CategoryVO> getSubCatList(Integer rootCatId) {
+        List<CategoryVO> subCatList = categoryMapperCustom.getSubCatList(rootCatId);
+        return subCatList;
     }
 }
