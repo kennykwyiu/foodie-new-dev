042d14086d622049f54f8df181d983d4eb5d8f5a
kenny <kenny.kwyiu@gmail.com>
2024-09-14 22:41:02 +0800

    getSixNewItemsLazy in mapper

--- foodie-mapper/src/main/java/com/kenny/mapper/CategoryMapperCustom.java
+++ foodie-mapper/src/main/java/com/kenny/mapper/CategoryMapperCustom.java
@@ -1,11 +1,12 @@
 package com.kenny.mapper;
 
-import com.kenny.my.mapper.MyMapper;
-import com.kenny.pojo.Category;
 import com.kenny.vo.CategoryVO;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface CategoryMapperCustom {
     public List<CategoryVO> getSubCatList(Integer rootCatId);
+    public List getSixNewItemsLazy(@Param("paramsMap") Map<String, Object> map);
 }
