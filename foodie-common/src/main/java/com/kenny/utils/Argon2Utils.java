b4838b9b81ad107313c2c206211e7e6161e19b97
kenny <kenny.kwyiu@gmail.com>
2024-09-15 11:13:53 +0800

    Argon2Utils - hashPassword

--- foodie-common/src/main/java/com/kenny/utils/Argon2Utils.java
+++ foodie-common/src/main/java/com/kenny/utils/Argon2Utils.java
@@ -14,7 +14,24 @@ public class Argon2Utils {
     static final int MEM_LIMIT = 66536;
     static final int HASH_LENGTH = 32;
     static final int PARALLELISM = 1;
+    public static String hashPassword(String password) {
+        byte[] salt = generateSalt16Byte();
 
+        Argon2Parameters.Builder builder = new Argon2Parameters.Builder(Argon2Parameters.ARGON2_i)
+                .withVersion(Argon2Parameters.ARGON2_VERSION_13)
+                .withIterations(ITERATIONS)
+                .withMemoryAsKB(MEM_LIMIT)
+                .withParallelism(PARALLELISM)
+                .withSalt(salt);
+
+        Argon2BytesGenerator generator = new Argon2BytesGenerator();
+        generator.init(builder.build());
+
+        byte[] passwordBytes = password.getBytes(StandardCharsets.UTF_8);
+        byte[] result = new byte[HASH_LENGTH];
+        generator.generateBytes(passwordBytes, result, 0, result.length);
+        return Hex.toHexString(salt) + Hex.toHexString(result);
+    }
 
     private static byte[] generateSalt16Byte() {
         SecureRandom secureRandom = new SecureRandom();
