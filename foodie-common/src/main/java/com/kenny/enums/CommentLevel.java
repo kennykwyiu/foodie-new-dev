d7f8285aa44a0d3ddbc497a12849540dfe4dd412
kenny <kenny.kwyiu@gmail.com>
2024-09-15 15:38:56 +0800

    CommentLevel

---
+++ foodie-common/src/main/java/com/kenny/enums/CommentLevel.java
@@ -0,0 +1,14 @@
+package com.kenny.enums;
+
+public enum CommentLevel {
+    GOOD(1, "GOOD"),
+    NORMAL(2, "NORMAL"),
+    BAD(3, "BAD");
+    public final Integer type;
+    public final String value;
+
+    CommentLevel(Integer type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+}
