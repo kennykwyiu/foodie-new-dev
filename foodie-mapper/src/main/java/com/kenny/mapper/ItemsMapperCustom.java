571ced4ad1eab4ad1da1ad968d55a1d8480fc8b1
kenny <kenny.kwyiu@gmail.com>
2024-09-16 22:26:17 +0800

    ItemsMapperCustom and mapper

---
+++ foodie-mapper/src/main/java/com/kenny/mapper/ItemsMapperCustom.java
@@ -0,0 +1,11 @@
+package com.kenny.mapper;
+
+import com.kenny.vo.ItemCommentVO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ItemsMapperCustom {
+    public List<ItemCommentVO> queryItemComments(@Param("paramsMap") Map<String, Object> map);
+}
