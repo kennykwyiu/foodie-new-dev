08f6adc678b5fc1180372ea8965e92c4464c5b3e
kenny <kenny.kwyiu@gmail.com>
2024-09-17 18:02:58 +0800

    searchItems im mapper

--- foodie-mapper/src/main/java/com/kenny/mapper/ItemsMapperCustom.java
+++ foodie-mapper/src/main/java/com/kenny/mapper/ItemsMapperCustom.java
@@ -1,6 +1,7 @@
 package com.kenny.mapper;
 
 import com.kenny.vo.ItemCommentVO;
+import com.kenny.vo.SearchItemsVO;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -8,4 +9,5 @@ import java.util.Map;
 
 public interface ItemsMapperCustom {
     public List<ItemCommentVO> queryItemComments(@Param("paramsMap") Map<String, Object> map);
+    public List<SearchItemsVO> searchItems(@Param("paramsMap") Map<String, Object> map);
 }
