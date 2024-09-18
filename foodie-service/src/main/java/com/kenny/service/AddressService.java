2d1d6ca91e26073ececa56640819c275d4a4144a
kenny <kenny.kwyiu@gmail.com>
2024-09-18 20:36:26 +0800

    queryAl and addNewUserAddress in AddressService

---
+++ foodie-service/src/main/java/com/kenny/service/AddressService.java
@@ -0,0 +1,12 @@
+package com.kenny.service;
+
+import com.kenny.bo.AddressBO;
+import com.kenny.pojo.UserAddress;
+
+import java.util.List;
+
+public interface AddressService {
+    public List<UserAddress> queryAll(String userId);
+    public void addNewUserAddress(AddressBO addressBO);
+
+}
---
+++ foodie-service/src/main/java/com/kenny/service/impl/AddressServiceImpl.java
@@ -0,0 +1,57 @@
+package com.kenny.service.impl;
+
+import com.kenny.bo.AddressBO;
+import com.kenny.mapper.UserAddressMapper;
+import com.kenny.pojo.UserAddress;
+import com.kenny.service.AddressService;
+import org.n3r.idworker.Sid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+    @Autowired
+    private UserAddressMapper userAddressMapper;
+
+    @Autowired
+    private Sid sid;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<UserAddress> queryAll(String userId) {
+        UserAddress ua = new UserAddress();
+        ua.setUserId(userId);
+
+        return userAddressMapper.select(ua);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public void addNewUserAddress(AddressBO addressBO) {
+        Integer isDefault = 0;
+        List<UserAddress> addressList = this.queryAll(addressBO.getUserId());
+        if (addressList == null || addressList.isEmpty() || addressList.size() == 0) {
+            isDefault = 1;
+        }
+
+        String addressId = sid.nextShort();
+
+        UserAddress newAddress = new UserAddress();
+        BeanUtils.copyProperties(addressBO, newAddress);
+
+        newAddress.setId(addressId);
+        newAddress.setIsDefault(isDefault);
+        newAddress.setCreatedTime(new Date());
+        newAddress.setUpdatedTime(new Date());
+
+        userAddressMapper.insert(newAddress);
+    }
+
+
+}
