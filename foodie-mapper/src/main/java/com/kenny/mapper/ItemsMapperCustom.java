20fb6000155f6be6d144c6e0273fc766fc14efd6
kenny <kenny.kwyiu@gmail.com>
2024-09-20 20:55:51 +0800

    decreaseItemSpecStock in ItemsMapperCustom.xml

--- foodie-mapper/src/main/java/com/kenny/mapper/ItemsMapperCustom.java
+++ foodie-mapper/src/main/java/com/kenny/mapper/ItemsMapperCustom.java
@@ -13,4 +13,6 @@ public interface ItemsMapperCustom {
     public List<SearchItemsVO> searchItems(@Param("paramsMap") Map<String, Object> map);
     public List<SearchItemsVO> searchItemsByThirdCat(@Param("paramsMap") Map<String, Object> map);
     public List<ShopcartVO> queryItemsBySpecIds(@Param("paramsList") List<String> specIdsList);
+    public int decreaseItemSpecStock(@Param("specId") String specId,
+                                     @Param("pendingCounts") int pendingCounts);
 }
