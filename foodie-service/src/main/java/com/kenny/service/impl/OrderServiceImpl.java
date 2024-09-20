dd4dd3b5ab50b9a2e382a59f8835a3c42a570c91
kenny <kenny.kwyiu@gmail.com>
2024-09-20 21:56:13 +0800

    implement createOrder

--- foodie-service/src/main/java/com/kenny/service/impl/OrderServiceImpl.java
+++ foodie-service/src/main/java/com/kenny/service/impl/OrderServiceImpl.java
@@ -1,18 +1,37 @@
 package com.kenny.service.impl;
 
 import com.kenny.bo.SubmitOrderBO;
+import com.kenny.enums.OrderStatusEnum;
+import com.kenny.enums.YesOrNo;
+import com.kenny.mapper.OrderItemsMapper;
+import com.kenny.mapper.OrderStatusMapper;
 import com.kenny.mapper.OrdersMapper;
+import com.kenny.pojo.*;
+import com.kenny.service.AddressService;
+import com.kenny.service.ItemService;
 import com.kenny.service.OrderService;
+import org.n3r.idworker.Sid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
+
 @Service
 public class OrderServiceImpl implements OrderService {
-
+    @Autowired
+    private AddressService addressService;
+    @Autowired
+    private ItemService itemService;
     @Autowired
     private OrdersMapper ordersMapper;
+    @Autowired
+    private OrderStatusMapper orderStatusMapper;
+    @Autowired
+    private OrderItemsMapper orderItemsMapper;
+    @Autowired
+    private Sid sid;
     @Transactional(propagation = Propagation.REQUIRED)
     @Override
     public void createOrder(SubmitOrderBO submitOrderBO) {
@@ -24,22 +43,79 @@ public class OrderServiceImpl implements OrderService {
 
         // default 0
         Integer postAmount = 0;
+        String orderId = sid.nextShort();
+        UserAddress address = addressService.queryUserAddress(userId, addressId);
 
         // 1. Save new order data
+        Orders newOrder = new Orders();
+        newOrder.setId(orderId);
+        newOrder.setUserId(userId);
+        newOrder.setReceiverName(address.getReceiver());
+        newOrder.setReceiverMobile(address.getMobile());
+        newOrder.setReceiverAddress(address.getProvince() + " "
+                + address.getCity() + " "
+                + address.getDistrict() + " "
+                + address.getDetail());
+//        newOrder.setTotalAmount();
+//        newOrder.setRealPayAmount();
+        newOrder.setPostAmount(postAmount);
+
+        newOrder.setPayMethod(payMethod);
+        newOrder.setLeftMsg(leftMsg);
+
+        newOrder.setIsComment(YesOrNo.NO.type);
+        newOrder.setIsDelete(YesOrNo.NO.type);
+        newOrder.setCreatedTime(new Date());
+        newOrder.setUpdatedTime(new Date());
 
         // 2. Loop through itemSpecIds to save order item information table
+        String[] itemSpecIdArr = itemSpecIds.split(",");
+        Integer totalAmount = 0;
+        Integer realPayAmount = 0;
 
         // TODO After integrating Redis, retrieve the purchased quantity from the Redis shopping cart
+        int buyCounts = 1;
 
-        // 2.1 Query specific information based on spec id, mainly to get the price
+        for (String itemSpecId : itemSpecIdArr) {
 
-        // 2.2 Get product information and product images based on product id
+            // 2.1 Query specific information based on spec id, mainly to get the price
+            ItemsSpec itemsSpec = itemService.queryItemSpecById(itemSpecId);
+            totalAmount += itemsSpec.getPriceNormal() * buyCounts;
+            realPayAmount += itemsSpec.getPriceDiscount() * buyCounts;
 
-        // 2.3 Loop to save sub-order data to the database
+            // 2.2 Get product information and product images based on product id
+            String itemId = itemsSpec.getItemId();
+            Items item = itemService.queryItemById(itemId);
+            String imgUrl = itemService.queryItemMainImgById(itemId);
 
-        // 2.4 After the user submits the order, deduct the inventory in the spec table
+            // 2.3 Loop to save sub-order data to the database
+            String subOrderId = sid.nextShort();
+            OrderItems subOrderItem = new OrderItems();
+            subOrderItem.setId(subOrderId);
+            subOrderItem.setOrderId(orderId);
+            subOrderItem.setItemId(itemId);
+            subOrderItem.setItemName(item.getItemName());
+            subOrderItem.setItemImg(imgUrl);
+            subOrderItem.setBuyCounts(buyCounts);
+            subOrderItem.setItemSpecId(itemSpecId);
+            subOrderItem.setItemSpecName(itemsSpec.getName());
+            subOrderItem.setPrice(itemsSpec.getPriceDiscount());
+            orderItemsMapper.insert(subOrderItem);
+
+            // 2.4 After the user submits the order, deduct the inventory in the spec table
+            itemService.decreaseItemSpecStock(itemSpecId, buyCounts);
+
+        }
+        newOrder.setTotalAmount(totalAmount);
+        newOrder.setRealPayAmount(realPayAmount);
+        ordersMapper.insert(newOrder);
 
         // 3. Save the order status table
+        OrderStatus waitPayOrderStatus = new OrderStatus();
+        waitPayOrderStatus.setOrderId(orderId);
+        waitPayOrderStatus.setOrderStatus(OrderStatusEnum.WAIT_PAY.type);
+        waitPayOrderStatus.setCreatedTime(new Date());
+        orderStatusMapper.insert(waitPayOrderStatus);
 
         // 4. Build merchant order to send to the payment center
 
