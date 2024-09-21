fdc5969a242c7f23065c7da22a395bd034f9ad71
kenny <kenny.kwyiu@gmail.com>
2024-09-21 11:17:07 +0800

    clear cookies

--- foodie-api/src/main/java/com/kenny/controller/OrdersController.java
+++ foodie-api/src/main/java/com/kenny/controller/OrdersController.java
@@ -2,6 +2,7 @@ package com.kenny.controller;
 
 import com.kenny.bo.SubmitOrderBO;
 import com.kenny.service.OrderService;
+import com.kenny.utils.CookieUtils;
 import com.kenny.utils.JsonResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -11,18 +12,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 
 @Api(value = "Order related", tags = {"API interfaces related to orders"})
 @RequestMapping("orders")
 @RestController
-public class OrdersController {
+public class OrdersController extends BaseController {
 
     @Autowired
     private OrderService orderService;
 
     @ApiOperation(value = "User Order", notes = "User Order", httpMethod = "POST")
     @PostMapping("/create")
-    public JsonResult create(@RequestBody SubmitOrderBO submitOrderBO) {
+    public JsonResult create(@RequestBody SubmitOrderBO submitOrderBO,
+                             HttpServletRequest request,
+                             HttpServletResponse response) {
 
 //        if (submitOrderBO.getPayMethod() != PayMethod.WEIXIN.type && submitOrderBO.getPayMethod() != PayMethod.ALIPAY.type) {
 //            return JsonResult.errorMsg("Payment method not supported!");
@@ -30,15 +36,16 @@ public class OrdersController {
 
         System.out.println(submitOrderBO.toString());
         // 1. Create order
-        orderService.createOrder(submitOrderBO);
+        String orderId = orderService.createOrder(submitOrderBO);
 
         // 2. After creating the order, remove the items that have been settled (submitted) from the shopping cart
+        // TODO After integrat ing Redis, enhance the functionality to clear settled items from the shopping cart and synchronize this update to the frontend cookie
+//        CookieUtils.setCookie(request, response, FOODIE_SHOPCART, "", true);
 
         // 3. Send the current order to the payment center to save the order data
 
 
-
-        return JsonResult.ok();
+        return JsonResult.ok(orderId);
     }
 
 }
