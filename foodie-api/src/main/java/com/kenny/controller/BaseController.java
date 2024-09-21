c5d4208087ba333477640785c40dc8b263dda15a
kenny <kenny.kwyiu@gmail.com>
2024-09-21 12:10:49 +0800

    added url in BaseController

--- foodie-api/src/main/java/com/kenny/controller/BaseController.java
+++ foodie-api/src/main/java/com/kenny/controller/BaseController.java
@@ -5,6 +5,14 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class BaseController {
 
+    // Payment center's invocation URL
+    String paymentUrl = "http://localhost:8088/foodie-payment/payment/createMerchantOrder"; // Production
+
+    // WeChat payment success -> Payment center -> Daily Gourmet Platform
+    //                                  |-> Callback notification URL
+//    String payReturnUrl = "http://api.z.mukewang.com/foodie-dev-api/orders/notifyMerchantOrderPaid";
+    String payReturnUrl = "http://localhost:8088/orders/notifyMerchantOrderPaid";
+
     public static final String FOODIE_SHOPCART = "shopcart";
     public static final Integer COMMON_PAGE_SIZE = 10;
     public static final Integer PAGE_SIZE = 20;
