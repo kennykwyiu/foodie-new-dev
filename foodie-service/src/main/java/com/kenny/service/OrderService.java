c18cd2036925d5a1663316f13ee1d68d7891169b
kenny <kenny.kwyiu@gmail.com>
2024-09-21 18:48:58 +0800

    queryOrderStatusInfo in OrderServiceImpl

--- foodie-service/src/main/java/com/kenny/service/OrderService.java
+++ foodie-service/src/main/java/com/kenny/service/OrderService.java
@@ -1,9 +1,11 @@
 package com.kenny.service;
 
 import com.kenny.bo.SubmitOrderBO;
+import com.kenny.pojo.OrderStatus;
 import com.kenny.vo.OrderVO;
 
 public interface OrderService {
     public OrderVO createOrder(SubmitOrderBO submitOrderBO);
     public void updateOrderStatus(String orderId, Integer orderStatus);
+    public OrderStatus queryOrderStatusInfo(String orderId);
 }
--- foodie-service/src/main/java/com/kenny/service/impl/OrderServiceImpl.java
+++ foodie-service/src/main/java/com/kenny/service/impl/OrderServiceImpl.java
@@ -143,4 +143,10 @@ public class OrderServiceImpl implements OrderService {
 
         orderStatusMapper.updateByPrimaryKeySelective(paidStatus);
     }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public OrderStatus queryOrderStatusInfo(String orderId) {
+        return orderStatusMapper.selectByPrimaryKey(orderId);
+    }
 }
