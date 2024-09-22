038fd012d7e82a68603c6f6cde901374db929d41
kenny <kenny.kwyiu@gmail.com>
2024-09-22 15:49:38 +0800

    updateUserInfo in CenterUserServiceImpl

--- foodie-service/src/main/java/com/kenny/service/center/CenterUserService.java
+++ foodie-service/src/main/java/com/kenny/service/center/CenterUserService.java
@@ -1,7 +1,10 @@
 package com.kenny.service.center;
 
+import com.kenny.bo.CenterUserBO;
 import com.kenny.pojo.Users;
 
 public interface CenterUserService {
     public Users queryUserInfo(String userId);
+
+    public Users updateUserInfo(String userId, CenterUserBO centerUserBO);
 }
--- foodie-service/src/main/java/com/kenny/service/impl/center/CenterUserServiceImpl.java
+++ foodie-service/src/main/java/com/kenny/service/impl/center/CenterUserServiceImpl.java
@@ -1,12 +1,17 @@
 package com.kenny.service.impl.center;
 
+import com.kenny.bo.CenterUserBO;
 import com.kenny.mapper.UsersMapper;
 import com.kenny.pojo.Users;
 import com.kenny.service.center.CenterUserService;
+import org.n3r.idworker.Sid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 @Service
 public class CenterUserServiceImpl implements CenterUserService {
@@ -14,11 +19,27 @@ public class CenterUserServiceImpl implements CenterUserService {
     @Autowired
     public UsersMapper usersMapper;
 
+    @Autowired
+    private Sid sid;
+
     @Transactional(propagation = Propagation.SUPPORTS)
     @Override
     public Users queryUserInfo(String userId) {
         Users user = usersMapper.selectByPrimaryKey(userId);
         user.setPassword(null);
         return user;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public Users updateUserInfo(String userId, CenterUserBO centerUserBO) {
+        Users updateUser = new Users();
+        BeanUtils.copyProperties(centerUserBO, updateUser);
+        updateUser.setId(userId);
+        updateUser.setUpdatedTime(new Date());
+
+        usersMapper.updateByPrimaryKeySelective(updateUser);
+
+        return queryUserInfo(userId);
     }
 }
