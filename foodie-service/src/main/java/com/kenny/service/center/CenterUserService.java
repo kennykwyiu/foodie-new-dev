b4a90371004b7f06e14257fce57cdb2236ea0f18
kenny <kenny.kwyiu@gmail.com>
2024-09-22 15:35:21 +0800

    queryUserInfo in CenterUserServiceImpl

---
+++ foodie-service/src/main/java/com/kenny/service/center/CenterUserService.java
@@ -0,0 +1,7 @@
+package com.kenny.service.center;
+
+import com.kenny.pojo.Users;
+
+public interface CenterUserService {
+    public Users queryUserInfo(String userId);
+}
---
+++ foodie-service/src/main/java/com/kenny/service/impl/center/CenterUserServiceImpl.java
@@ -0,0 +1,24 @@
+package com.kenny.service.impl.center;
+
+import com.kenny.mapper.UsersMapper;
+import com.kenny.pojo.Users;
+import com.kenny.service.center.CenterUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CenterUserServiceImpl implements CenterUserService {
+
+    @Autowired
+    public UsersMapper usersMapper;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public Users queryUserInfo(String userId) {
+        Users user = usersMapper.selectByPrimaryKey(userId);
+        user.setPassword(null);
+        return user;
+    }
+}
