900918a9d88f94dbc105886315b95683dc0b11b6
kenny <kenny.kwyiu@gmail.com>
2024-09-22 16:57:39 +0800

    update api in CenterUserController - add @Valid and BindingResult

--- foodie-api/src/main/java/com/kenny/controller/center/CenterUserController.java
+++ foodie-api/src/main/java/com/kenny/controller/center/CenterUserController.java
@@ -9,10 +9,16 @@ import com.kenny.utils.JsonUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Api(value = "User Information API", tags = {"APIs related to user information"})
 @RestController
@@ -23,9 +29,15 @@ public class CenterUserController {
     @PostMapping("/update")
     public JsonResult update(@ApiParam(name = "userId", value = "用户id", required = true)
                              @RequestParam String userId,
-                             @RequestBody CenterUserBO centerUserBO,
+                             @RequestBody @Valid CenterUserBO centerUserBO,
+                             BindingResult result,
                              HttpServletRequest request, HttpServletResponse response) {
         System.out.println(centerUserBO);
+
+        if (result.hasErrors()) {
+            Map<String, String> errorMap = getErrors(result);
+            return JsonResult.errorMap(errorMap);
+        }
 
         Users userResult = centerUserService.updateUserInfo(userId, centerUserBO);
         userResult = setNullProperty(userResult);
@@ -34,6 +46,17 @@ public class CenterUserController {
         // TODO: Future improvement - Add token authentication, integrate with Redis for distributed sessions
 
         return JsonResult.ok();
+    }
+
+    private Map<String, String> getErrors(BindingResult result) {
+        Map<String, String> map = new HashMap<>();
+        List<FieldError> errorList = result.getFieldErrors();
+        for (FieldError error : errorList) {
+            String errorField = error.getField();
+            String errorMsg = error.getDefaultMessage();
+            map.put(errorField,errorMsg);
+        }
+        return map;
     }
 
     private Users setNullProperty(Users userResult) {
