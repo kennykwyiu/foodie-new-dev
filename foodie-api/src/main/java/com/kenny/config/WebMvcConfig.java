c080964ec5f0210aa705ddd5fa7e45bcd11183e8
kenny <kenny.kwyiu@gmail.com>
2024-09-23 22:36:24 +0800

    addResourceHandlers in WebMvcConfig

--- foodie-api/src/main/java/com/kenny/config/WebMvcConfig.java
+++ foodie-api/src/main/java/com/kenny/config/WebMvcConfig.java
@@ -4,13 +4,22 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/META-INF/resources/") // reflect to swagger 2
+                .addResourceLocations("file:\\C:\\Users\\yaowi\\Desktop\\java\\images\\");
+        // -> http://localhost:8088/foodie/faces/1908017YR51G1XWH/face-1908017YR51G1XWH.png
+    }
 }
