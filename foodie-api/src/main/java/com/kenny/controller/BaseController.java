3c937a8dc6188cba039fb63acb0dc3443234723d
kenny <kenny.kwyiu@gmail.com>
2024-09-23 21:53:22 +0800

    added IMAGE_USER_FACE_LOCATION into BaseController

--- foodie-api/src/main/java/com/kenny/controller/BaseController.java
+++ foodie-api/src/main/java/com/kenny/controller/BaseController.java
@@ -2,6 +2,8 @@ package com.kenny.controller;
 
 import org.springframework.stereotype.Controller;
 
+import java.io.File;
+
 @Controller
 public class BaseController {
 
@@ -17,4 +19,17 @@ public class BaseController {
     public static final String FOODIE_SHOPCART = "shopcart";
     public static final Integer COMMON_PAGE_SIZE = 10;
     public static final Integer PAGE_SIZE = 20;
+
+    // Location for user avatar uploads
+//    public static final String IMAGE_USER_FACE_LOCATION = File.separator + "workspaces" +
+//            File.separator + "images" +
+//            File.separator + "foodie" +
+//            File.separator + "faces";
+    //public static final String IMAGE_USER_FACE_LOCATION = "C:\Users\yaowi\Desktop\java\images\foodie\faces";
+
+    public static final String IMAGE_USER_FACE_LOCATION = "C:" + File.separator + "Users" + File.separator + "yaowi" +
+            File.separator + "Desktop" + File.separator + "java" +
+            File.separator + "images" + File.separator + "foodie" +
+            File.separator + "faces";
+
 }
