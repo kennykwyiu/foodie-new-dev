12ff4f3466796e71d64cc7860a810d7a2757ab99
kenny <kenny.kwyiu@gmail.com>
2024-09-23 21:53:56 +0800

    uploadFace in CenterUserController

--- foodie-api/src/main/java/com/kenny/controller/center/CenterUserController.java
+++ foodie-api/src/main/java/com/kenny/controller/center/CenterUserController.java
@@ -1,21 +1,28 @@
 package com.kenny.controller.center;
 
 import com.kenny.bo.CenterUserBO;
+import com.kenny.controller.BaseController;
 import com.kenny.pojo.Users;
 import com.kenny.service.center.CenterUserService;
 import com.kenny.utils.CookieUtils;
 import com.kenny.utils.JsonResult;
 import com.kenny.utils.JsonUtils;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,9 +30,53 @@ import java.util.Map;
 @Api(value = "User Information API", tags = {"APIs related to user information"})
 @RestController
 @RequestMapping("userInfo")
-public class CenterUserController {
+public class CenterUserController extends BaseController {
     @Autowired
     private CenterUserService centerUserService;
+
+    @ApiOperation(value = "User avatar update", notes = "User avatar update", httpMethod = "POST")
+    @PostMapping("uploadFace")
+    public JsonResult uploadFace(@ApiParam(name = "userId", value = "User ID", required = true)
+                                 @RequestParam String userId,
+                                 @ApiParam(name = "file", value = "User avatar", required = true)
+                                 MultipartFile file,
+                                 HttpServletRequest request, HttpServletResponse response) {
+
+        String fileSpace = IMAGE_USER_FACE_LOCATION;
+
+        String uploadPathPrefix = File.separator + userId;
+
+        if (file != null) {
+            FileOutputStream fileOutputStream = null;
+
+            try {
+                String filename = file.getOriginalFilename();
+                if (StringUtils.isNoneBlank(filename)) {
+                    String[] fileNameArr = filename.split("\\.");
+                    String suffix = fileNameArr[fileNameArr.length - 1];
+                    if (!suffix.equalsIgnoreCase("png") &&
+                            !suffix.equalsIgnoreCase("jpg") &&
+                            !suffix.equalsIgnoreCase("jpeg")) {
+                        return JsonResult.errorMsg("Incorrect image format!");
+                    }
+
+                }
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+            }
+
+
+        } else {
+            return JsonResult.errorMsg("File cannot be empty!");
+        }
+
+
+        return JsonResult.ok();
+    }
+
+    @ApiOperation(value = "Modify user information", notes = "Modify user information", httpMethod = "POST")
     @PostMapping("/update")
     public JsonResult update(@ApiParam(name = "userId", value = "用户id", required = true)
                              @RequestParam String userId,
@@ -54,7 +105,7 @@ public class CenterUserController {
         for (FieldError error : errorList) {
             String errorField = error.getField();
             String errorMsg = error.getDefaultMessage();
-            map.put(errorField,errorMsg);
+            map.put(errorField, errorMsg);
         }
         return map;
     }
