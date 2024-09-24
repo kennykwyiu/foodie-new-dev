1e1835b74de6347ddf9b42c424feab1dfa28d8ea
kenny <kenny.kwyiu@gmail.com>
2024-09-24 23:13:33 +0800

    getSixNewItemsLazy - fixed bug

--- foodie-mapper/src/main/java/com/kenny/mapper/CategoryMapperCustom.java
+++ foodie-mapper/src/main/java/com/kenny/mapper/CategoryMapperCustom.java
@@ -1,6 +1,7 @@
 package com.kenny.mapper;
 
 import com.kenny.vo.CategoryVO;
+import com.kenny.vo.NewItemsVO;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -8,5 +9,5 @@ import java.util.Map;
 
 public interface CategoryMapperCustom {
     public List<CategoryVO> getSubCatList(Integer rootCatId);
-    public List getSixNewItemsLazy(@Param("paramsMap") Map<String, Object> map);
+    public List<NewItemsVO> getSixNewItemsLazy(@Param("paramsMap") Map<String, Object> map);
 }
