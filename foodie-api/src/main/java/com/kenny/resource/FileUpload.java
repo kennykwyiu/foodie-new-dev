b2c6888112e9ceb1616409b4c64b23ae90f915d8
kenny <kenny.kwyiu@gmail.com>
2024-09-24 21:33:30 +0800

    imageServerUrl

--- foodie-api/src/main/java/com/kenny/resource/FileUpload.java
+++ foodie-api/src/main/java/com/kenny/resource/FileUpload.java
@@ -9,6 +9,15 @@ import org.springframework.stereotype.Component;
 @PropertySource("classpath:file-upload-dev.properties")
 public class FileUpload {
     private String imageUserFaceLocation;
+    private String imageServerUrl;
+
+    public String getImageServerUrl() {
+        return imageServerUrl;
+    }
+
+    public void setImageServerUrl(String imageServerUrl) {
+        this.imageServerUrl = imageServerUrl;
+    }
 
     public String getImageUserFaceLocation() {
         return imageUserFaceLocation;
