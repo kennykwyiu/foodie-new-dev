4b1142595d0a99408de3fed0a8573b9ab901818b
kenny <kenny.kwyiu@gmail.com>
2024-09-27 23:21:21 +0800

    queryMyComments in service

--- foodie-service/src/main/java/com/kenny/service/MyCommentsService.java
+++ foodie-service/src/main/java/com/kenny/service/MyCommentsService.java
@@ -1,10 +1,15 @@
 package com.kenny.service;
 
 import com.kenny.pojo.OrderItems;
+import com.kenny.utils.PagedGridResult;
 
 import java.util.List;
 
 public interface MyCommentsService {
     public List<OrderItems> queryPendingComments(String orderId);
+
+    PagedGridResult queryMyComments(String userId, Integer page, Integer pageSize);
+
+
 }
 
--- foodie-service/src/main/java/com/kenny/service/impl/MyCommentsServiceImpl.java
+++ foodie-service/src/main/java/com/kenny/service/impl/MyCommentsServiceImpl.java
@@ -1,19 +1,26 @@
 package com.kenny.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.kenny.mapper.ItemsCommentsMapperCustom;
 import com.kenny.mapper.OrderItemsMapper;
 import com.kenny.mapper.OrderStatusMapper;
 import com.kenny.mapper.OrdersMapper;
 import com.kenny.pojo.OrderItems;
 import com.kenny.service.MyCommentsService;
+import com.kenny.service.impl.center.BaseService;
+import com.kenny.utils.PagedGridResult;
+import com.kenny.vo.MyCommentVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
-public class MyCommentsServiceImpl implements MyCommentsService {
+public class MyCommentsServiceImpl extends BaseService implements MyCommentsService {
 
     @Autowired
     public OrderItemsMapper orderItemsMapper;
@@ -23,6 +30,9 @@ public class MyCommentsServiceImpl implements MyCommentsService {
 
     @Autowired
     public OrderStatusMapper orderStatusMapper;
+    @Autowired
+    public ItemsCommentsMapperCustom itemsCommentsMapperCustom;
+
 
     @Transactional(propagation = Propagation.SUPPORTS)
     @Override
@@ -30,5 +40,20 @@ public class MyCommentsServiceImpl implements MyCommentsService {
         OrderItems query = new OrderItems();
         query.setOrderId(orderId);
         return orderItemsMapper.select(query);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public PagedGridResult queryMyComments(String userId,
+                                           Integer page,
+                                           Integer pageSize) {
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("userId", userId);
+
+        PageHelper.startPage(page, pageSize);
+        List<MyCommentVO> list = itemsCommentsMapperCustom.queryMyComments(map);
+
+        return setterPagedGrid(list, page);
     }
 }
