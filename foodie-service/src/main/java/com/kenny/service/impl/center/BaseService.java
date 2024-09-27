49a4bd0baeae67131fb97a8afaf8e2046dfe9a07
kenny <kenny.kwyiu@gmail.com>
2024-09-27 23:20:30 +0800

    setterPagedGrid in BaseService

---
+++ foodie-service/src/main/java/com/kenny/service/impl/center/BaseService.java
@@ -0,0 +1,20 @@
+package com.kenny.service.impl.center;
+
+import com.github.pagehelper.PageInfo;
+import com.kenny.utils.PagedGridResult;
+
+import java.util.List;
+
+public class BaseService {
+
+    public PagedGridResult setterPagedGrid(List<?> list, Integer page) {
+        PageInfo<?> pageList = new PageInfo<>(list);
+        PagedGridResult grid = new PagedGridResult();
+        grid.setPage(page);
+        grid.setRows(list);
+        grid.setTotal(pageList.getPages());
+        grid.setRecords(pageList.getTotal());
+        return grid;
+    }
+
+}
