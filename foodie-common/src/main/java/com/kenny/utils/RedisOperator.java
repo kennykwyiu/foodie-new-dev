81ebabb13fb1acd067ef1a29db0cab8767dfd295
kenny <kenny.kwyiu@gmail.com>
2024-11-02 15:29:43 +0800

    mget in RedisOperator

--- foodie-common/src/main/java/com/kenny/utils/RedisOperator.java
+++ foodie-common/src/main/java/com/kenny/utils/RedisOperator.java
@@ -99,6 +99,10 @@ public class RedisOperator {
         return (String) redisTemplate.opsForValue().get(key);
     }
 
+    public List<String> get(List<String> keys) {
+        return redisTemplate.opsForValue().multiGet(keys);
+    }
+
     /**
      * Batch query, equivalent to mget
      *
