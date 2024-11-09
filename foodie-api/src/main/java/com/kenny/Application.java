b718186a9383b72fcdf7203256274d4ff1dfdbd4
kenny <kenny.kwyiu@gmail.com>
2024-11-09 15:34:10 +0800

    revise config in yml

--- foodie-api/src/main/java/com/kenny/Application.java
+++ foodie-api/src/main/java/com/kenny/Application.java
@@ -2,17 +2,15 @@ package com.kenny;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication//(exclude = {SecurityAutoConfiguration.class})
 @MapperScan(basePackages = "com.kenny.mapper")
 @ComponentScan(basePackages = {"com.kenny", "org.n3r.idworker"})
 @EnableScheduling
-@EnableRedisHttpSession
+//@EnableRedisHttpSession
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
