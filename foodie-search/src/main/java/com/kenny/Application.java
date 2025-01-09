4d9894fd22968014ead7c8d61751bb299b0d83d5
kenny <kenny.kwyiu@gmail.com>
2025-01-09 23:29:31 +0800

    add Application

---
+++ foodie-search/src/main/java/com/kenny/Application.java
@@ -0,0 +1,14 @@
+package com.kenny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
