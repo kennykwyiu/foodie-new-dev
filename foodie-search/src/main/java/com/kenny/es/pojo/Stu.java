24b2870ab2a3c92c2c222f811ba6f146a314c7f6
kenny <kenny.kwyiu@gmail.com>
2025-01-13 22:34:44 +0800

    add fields in Stu

--- foodie-search/src/main/java/com/kenny/es/pojo/Stu.java
+++ foodie-search/src/main/java/com/kenny/es/pojo/Stu.java
@@ -3,8 +3,9 @@ package com.kenny.es.pojo;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
-@Document(indexName = "stu", type = "_doc")
+@Document(indexName = "stu", type = "_doc", shards = 3, replicas = 0)
 public class Stu {
     @Id
     private Long stuId;
@@ -14,6 +15,15 @@ public class Stu {
 
     @Field(store = true)
     private Integer age;
+
+    @Field(store = true)
+    private String description;
+
+    @Field(store = true, type = FieldType.Keyword)
+    private String sign;
+
+    @Field(store = true)
+    private Float money;
 
     public Long getStuId() {
         return stuId;
@@ -37,5 +47,41 @@ public class Stu {
 
     public void setAge(Integer age) {
         this.age = age;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+    public Float getMoney() {
+        return money;
+    }
+
+    public void setMoney(Float money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        return "Stu{" +
+                "stuId=" + stuId +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", description='" + description + '\'' +
+                ", sign='" + sign + '\'' +
+                ", money=" + money +
+                '}';
     }
 }
