d4c908726e4d607b745c29541ac21367291940a5
kenny <kenny.kwyiu@gmail.com>
2025-02-09 17:51:51 +0800

    CorsConfig

---
+++ foodie-search/src/main/java/com/kenny/config/CorsConfig.java
@@ -0,0 +1,42 @@
+package com.kenny.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    public CorsConfig() {
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:8080");
+        configuration.addAllowedOrigin("http://192.168.74.128:8080");
+        configuration.addAllowedOrigin("http://192.168.74.128");
+        configuration.addAllowedOrigin("http://192.168.74.134");
+        configuration.addAllowedOrigin("http://192.168.74.134:6379");
+        configuration.addAllowedOrigin("http://www.kenny.com:8080");
+        configuration.addAllowedOrigin("http://www.kenny.com:8080/");
+        configuration.addAllowedOrigin("http://www.kenny.com:90/");
+        configuration.addAllowedOrigin("http://www.kenny.com:90");
+        configuration.addAllowedOrigin("http://www.kenny.com");
+        configuration.addAllowedOrigin("*");
+
+        // allow to send cookie info
+        configuration.setAllowCredentials(true);
+
+        // allow all request methods
+        configuration.addAllowedMethod("*");
+
+        configuration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        corsConfigurationSource.registerCorsConfiguration("/**", configuration);
+
+        return new CorsFilter(corsConfigurationSource);
+    }
+}
